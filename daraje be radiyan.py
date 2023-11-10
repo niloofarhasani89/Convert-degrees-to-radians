85d0615bb7e021aaa094bad3f7af461472200e5d
niloofarhasani89 <147707638+niloofarhasani89@users.noreply.github.com>
2023-11-10 11:33:55 +0330

    Update daraje be radiyan.py

--- daraje be radiyan.py
+++ daraje be radiyan.py
@@ -1,3 +1,3 @@
-daraje = int (input(" give me an angle : "))
-radiyan = daraje * (3.14 / 180)
-print (radiyan,"r")
+degree = int (input(" give me an angle : "))
+radian = degree * (3.14 / 180)
+print (radian,"r")
